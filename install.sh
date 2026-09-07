7c4b5f48ee5b67dcddeb6bc9f5b1d97c3efa5477
Caleb Tech <caleb111723alt@gmail.com>
2026-09-07 08:48:45 -0400

    Revise install.sh for kernel and app installations
    Updated installation script to install Liquorix kernel, Nix, and useful apps via pacman. Added configuration for Nix and user sudoer setup.

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
-sudo mkdir /logo
-sudo mv /home/caleb111723/Documents/logo.png /logo
-sudo LOGO="/logo/logo.png" sh -c 'grep -v "^LOGO=" /etc/os-release; echo "LOGO=$LOGO"' | sudo tee /etc/os-release > /dev/null
+echo 'installing kernel..'
 curl -s 'https://liquorix.net/install-liquorix.sh' | sudo bash
+echo 'installing nix..'
 curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install | sh -s -- --daemon
+echo 'fixing pacman..'
 sudo pacman -Sy archlinux-keyring
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
+echo 'installing usefull apps via pacman..'
 sudo pacman -S --noconfirm fastfetch
 sudo pacman -S --noconfirm nano
 sudo pacman -S --noconfirm vim
 sudo pacman -S --noconfirm git
+echo 'allowing flakes for nix..'
 sudo mkdir /home/user/.config/nix/
 sudo echo "experimental-features = nix-command flakes" >> /home/user/.config/nix/nix.conf
+echo 'making user sudoer..'
 echo "user ALL=(ALL:ALL) ALL" | sudo tee /etc/sudoers
 echo "Done! if you want firefox, restart your terminal and run 'nix profile add nixpkgs#firefox' if you get errors, try running with sudo, or still getting errors, make sure your running this as "caleb111723" account or if still getting errors, wait for the bug to be fixed, we are still making working on this script"
